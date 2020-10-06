c2549ffb4db0443b95bd70218c461aa507d2d63a
Magnus Sundström <magnus.sundstrom@b3.se>
2020-10-06 15:28:48 +0200

    Added word retrieval logic

--- DbService.cs
+++ DbService.cs
@@ -5,6 +5,24 @@ namespace WordGame
 {
     public class DbService
     {
+        public List<string> GetWords()
+        {
+            var conn = new SqlConnection("Server=.\\SQLEXPRESS;Database=wordgame;Integrated Security=true");
+            conn.Open();
 
+            SqlCommand cmd = new SqlCommand("SELECT word FROM words", conn);
+            SqlDataReader reader = cmd.ExecuteReader();
+            List<string> results = new List<string>();
+
+            while (reader.Read())
+            {
+                results.Add((string)reader["word"]);
+            }
+
+            reader.Close();
+            conn.Close();
+
+            return results;
+        }
     }
 }
